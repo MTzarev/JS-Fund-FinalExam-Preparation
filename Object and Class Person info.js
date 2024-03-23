c5bb1350870e79fb8355859772595196c2bbee58
Miroslav Tsarev <mtzarev@gmail.com>
2024-03-23 20:51:06 +0200

    Add files via upload

---
+++ Object and Class Person info.js
@@ -0,0 +1,11 @@
+function personInfo(name, surname, years) {
+    let info = {};
+    info.firstName = name;
+    info.lastName = surname;
+    info.age = years;
+ return info;
+}
+personInfo("Miroslav",
+    "Tzarev",
+    "40"
+);
